b105c2b06797c335fb6b4ec2e831dd07e8fa62bb
YomNom <joeyyyong@gmail.com>
2024-11-13 15:55:15 -0500

    Just some simple layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+
+    import EventForm from "./event_form.svelte";
+</script>
+
+<main class="main">
+    <nav>
+        <div class="navbar">
+            <img src="/UC-ReadoutLogo.png" alt="UC Readout Logo" class="UClogo" width="125px" height="50px"/>
+        </div>
+    </nav>
+
+    <div class="top-page">
+        <EventForm />
+    </div>
+</main>
+
+<style>
+    .main {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        font-size: 1.5rem;
+    }
+    .navbar {
+        background-color: #d91515;
+    }
+
+    .UClogo {
+        margin-left: 20px; /* Adjust the value as needed */
+    }
+    
+    .top-page {
+        float: left;
+        height: 40%;
+        padding: 10px;
+        box-sizing: border-box;
+        background-color: #afabab;
+    }
+</style>
