2acdf1d3e9fff105833dfcb4f594b1bd35f99b0e
YomNom <joeyyyong@gmail.com>
2024-11-14 02:04:43 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
     import EventForm from "./event_form.svelte";
 </script>
 
-<main class="main">
+<main>
     <nav>
         <div class="navbar">
             <img src="/UC-ReadoutLogo.png" alt="UC Readout Logo" class="UClogo" width="125px" height="50px"/>
@@ -17,13 +17,18 @@
 </main>
 
 <style>
-    .main {
+    main {
         display: flex;
         flex-direction: column;
+        width: 100%;
         height: 100vh;
         font-size: 1.5rem;
     }
     .navbar {
+        width: 100%;
+        position: fixed;
+        top: 0;
+        left: 0;
         background-color: #d91515;
     }
 
